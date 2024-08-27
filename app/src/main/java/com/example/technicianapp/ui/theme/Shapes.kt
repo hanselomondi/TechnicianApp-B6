374113602ef0458a4b7e1d694eff0e1cbb6371b2
Hansel Omondi <ogopabhs@gmail.com>
2024-08-27 22:30:06 +0300

    Add: material3 theme to app
    This is a blue theme in line with D&S's company colours

---
+++ app/src/main/java/com/example/technicianapp/ui/theme/Shapes.kt
@@ -0,0 +1,13 @@
+package com.example.technicianapp.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraLarge = RoundedCornerShape(24.dp),
+    large =  RoundedCornerShape(20.dp),
+    medium = RoundedCornerShape(16.dp),
+    small =  RoundedCornerShape(12.dp),
+    extraSmall = RoundedCornerShape(8.dp)
+)
